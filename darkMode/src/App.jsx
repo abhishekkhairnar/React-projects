e50e3c7c45d52777dd2a48e8aed996544562faa3
unknown <khairnarabhi25@gmail.com>
2024-02-04 16:57:53 +0530

    context-api

--- darkMode/src/App.jsx
+++ darkMode/src/App.jsx
@@ -1,6 +1,8 @@
 import "./App.css";
 import { ThemeProvider } from "./contexts/Theme";
-
+import ThemeButton from "./components/ThemeButton";
+import Card from "./components/Card";
+import { useState, useEffect } from "react";
 function App() {
   const [themeMode, setThemeMode] = useState("light")
 
@@ -23,11 +25,11 @@ function App() {
       <div className="flex flex-wrap min-h-screen items-center">
         <div className="w-full">
           <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-            
+            <ThemeButton/>
           </div>
 
           <div className="w-full max-w-sm mx-auto">
-
+            <Card/>
           </div>
         </div>
       </div>
